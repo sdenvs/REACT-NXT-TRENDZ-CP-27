5787d755cf1dbd2b0225e92138f0e582168564bc
sdenvs <sdenvs@gmail.com>
2021-12-27 12:13:06 +0530

    change-1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
-const App = () => <h1>Remove this heading and write your code here</h1>
+import {Route, Switch} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <Route exact path="/" component={Home} />
+    <Route component={NotFound} />
+  </Switch>
+)
 
 export default App
